88ba485ccad606b551edba34f9d74caf23990b32
Kailash Nadh <kailash@nadh.in>
2023-12-26 23:10:33 +0530

    Replace c3js with chart.js for major dep size reduction.

---
+++ frontend/src/components/DonutChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="donut-chart">
+    <canvas class="donut-chart-canvas" />
+  </section>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+const DEFAULT = {
+  type: 'doughnut',
+  data: {},
+  options: {
+    responsive: true,
+    cutout: '70%',
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        backgroundColor: '#fff',
+        borderColor: '#ddd',
+        borderWidth: 1,
+        titleColor: '#666',
+        bodyColor: '#666',
+        bodyFont: {
+          size: 15,
+        },
+        bodySpacing: 10,
+        padding: 10,
+      },
+    },
+  },
+};
+
+export default {
+  name: 'DonutChart',
+
+  props: {
+    data: { type: Object, default: () => { } },
+  },
+
+  mounted() {
+    const ctx = this.$el.querySelector('.donut-chart-canvas');
+    this.chart = new Chart(ctx, { ...DEFAULT, data: this.$props.data });
+  },
+};
+</script>
---
+++ frontend/src/components/LineChart.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="line-chart">
+    <canvas class="line-chart-canvas" />
+  </section>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+const DEFAULT = {
+  type: 'line',
+  data: {},
+  options: {
+    responsive: true,
+    lineTension: 0.5,
+    maintainAspectRatio: false,
+    interaction: {
+      intersect: false,
+      axis: 'index',
+    },
+    plugins: {
+      legend: {
+        // display: false,
+      },
+      tooltip: {
+        backgroundColor: '#fff',
+        borderColor: '#ddd',
+        borderWidth: 1,
+        bodyColor: '#666',
+        displayColors: true,
+        bodyFont: {
+          size: 15,
+        },
+        bodySpacing: 10,
+        padding: 10,
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+};
+
+export default {
+  name: 'LineChart',
+
+  props: {
+    data: { type: Object, default: () => { } },
+  },
+
+  mounted() {
+    const ctx = this.$el.querySelector('.line-chart-canvas');
+    this.chart = new Chart(ctx, { ...DEFAULT, data: this.$props.data });
+  },
+};
+</script>
